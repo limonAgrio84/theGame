b6698f0bd580c793ed62bb048ea5a5adc7caef90
Limon <151796984+limonAgrio84@users.noreply.github.com>
2026-08-07 10:41:25 -0500

    renderizando el render de player

--- src/player/player.py
+++ src/player/player.py
@@ -2,6 +2,13 @@ class Player:
     def __init__(self, game):
         self.game = game
         print("Player created")
+
+        self.model = self.game.loader.loadModel("models/misc/rgbCube")
+        self.model.reparentTo(self.game.render)
+        self.model.setScale(0.2)
+        self.model.setPos(0, 10, 0)
+
+
         
 
     def update(self):
