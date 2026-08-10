913b8c31dc4da534e60c24a4396a59ea140cb339
Limon <151796984+limonAgrio84@users.noreply.github.com>
2026-08-10 14:25:03 -0500

    agregando que el personaje rote en base a como se mueve

--- src/player/player.py
+++ src/player/player.py
@@ -1,4 +1,6 @@
 from panda3d.core import ClockObject
+from panda3d.core import Vec3
+import math
 class Player:
     def __init__(self, game):
         self.game = game
@@ -18,18 +20,36 @@ class Player:
     def update(self):
         dt = ClockObject.getGlobalClock().getDt()
 
+        self.move(dt)
+
+        
+
+    def move(self,dt):
+        direction = Vec3(0, 0, 0)
+
         if self.game.input_manager.is_key_pressed("w"):
-            self.model.setY(self.model.getY() + self.speed * dt)
-            print("Moving forward")
-
+                direction.y += 1
+                print("Moving forward")
+        
         if self.game.input_manager.is_key_pressed("s"):
-            self.model.setY(self.model.getY() - self.speed * dt)
+            direction.y -= 1
             print("Moving backward")
-
+        
         if self.game.input_manager.is_key_pressed('a'):
-            self.model.setX(self.model.getX() - self.speed * dt)
+            direction.x -= 1
             print("Moving left")
-
+        
         if self.game.input_manager.is_key_pressed('d'):
-            self.model.setX(self.model.getX() + self.speed * dt)
-            print("Moving right")
+            direction.x += 1
+            print("Moving right")    
+
+        if direction.length() > 0:
+             direction.normalize()
+
+        #calcular la rotacion del modelo en base a la direccion 
+        angle = math.degrees(math.atan2(direction.x, direction.y))
+        self.model.setH(angle)
+
+        #mover     
+        movement = direction * self.speed * dt
+        self.model.setPos(self.model.getPos() + movement)
