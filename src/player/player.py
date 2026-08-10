f35ccb59aaa38c07cc0275f34cc57fa44e6e38b0
Limon <151796984+limonAgrio84@users.noreply.github.com>
2026-08-10 13:43:58 -0500

    haciendo que se mueva el cubo

--- src/player/player.py
+++ src/player/player.py
@@ -1,3 +1,4 @@
+from panda3d.core import ClockObject
 class Player:
     def __init__(self, game):
         self.game = game
@@ -8,9 +9,27 @@ class Player:
         self.model.setScale(0.2)
         self.model.setPos(0, 10, 0)
 
+        #velocidad
+        self.speed = 5.0
+
 
         
 
     def update(self):
+        dt = ClockObject.getGlobalClock().getDt()
+
         if self.game.input_manager.is_key_pressed("w"):
+            self.model.setY(self.model.getY() + self.speed * dt)
             print("Moving forward")
+
+        if self.game.input_manager.is_key_pressed("s"):
+            self.model.setY(self.model.getY() - self.speed * dt)
+            print("Moving backward")
+
+        if self.game.input_manager.is_key_pressed('a'):
+            self.model.setX(self.model.getX() - self.speed * dt)
+            print("Moving left")
+
+        if self.game.input_manager.is_key_pressed('d'):
+            self.model.setX(self.model.getX() + self.speed * dt)
+            print("Moving right")
